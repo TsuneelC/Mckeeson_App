6c4331eb0f69d95d65bcfd62610c2c760f4b802d
TsuneelC <47574227+TsuneelC@users.noreply.github.com>
2019-02-14 21:08:10 -0500

    updated includ and exclude drop down

--- src/Components/CommonAttributes.js
+++ src/Components/CommonAttributes.js
@@ -1,18 +1,13 @@
-
 import React, { Component, Fragment } from 'react';
-
 
 export const CustomerAttributeselector = (props) => {
     return (
-
         <div className="col-sm-2" >
-
             <select id="customeroption" className="browser-default camp-select" onChange={props.handlecustomerattributelabel}>
                 <option defaultValue>Choose An Attribute..</option>
                 <option value="group">Group</option>
                 <option value="billto">Billto</option>
                 <option value="shipto">Shipto</option>
-
             </select>
         </div>
     );
@@ -20,7 +15,6 @@ export const CustomerAttributeselector = (props) => {
 
 export const ItemAttributeselector = (props) => {
     return (
-
         <div className="col-sm-2" >
 
             <select id="itemoption" className="browser-default camp-select" onChange={props.handleitemattributelabel}>
@@ -28,11 +22,19 @@ export const ItemAttributeselector = (props) => {
                 <option value="group">Group</option>
                 <option value="billto">Billto</option>
                 <option value="shipto">Shipto</option>
-
             </select>
-
-
         </div>
+    );
+};
 
+export const OperatorAttributeselector = (props) => {
+    return (
+        <div className="col-sm-2" >
+            <select id="operatoroption" className="browser-default camp-select" onChange={props.handleoperatorattribute}>
+                <option defaultValue>Choose An Operation..</option>
+                <option value="included">Include</option>
+                <option value="excluded">Exclude</option>
+            </select>
+        </div>
     );
 };
